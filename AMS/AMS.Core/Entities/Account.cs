71a7b0fd57e6191e31626789e06b85d8fe00d92f
mhpranta12 <mhpranta2@gmail.com>
2025-06-26 19:54:49 +0600

    Added Entities

---
+++ AMS/AMS.Core/Entities/Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMS.Core.Entities
+{
+    public class Account
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string UserType { get; set; }
+        public decimal? Cash { get; set; }
+        public decimal? Recieveable { get; set; }
+        public Guid? ParentAccountId { get; set; }
+    }
+}
