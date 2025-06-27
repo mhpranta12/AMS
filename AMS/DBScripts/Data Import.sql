b5efddfeaa4abfac9ea31bbe6127b7f0639f4b28
mhpranta12 <mhpranta2@gmail.com>
2025-06-27 23:23:03 +0600

    minor fix

--- AMS/DBScripts/Data Import.sql
+++ AMS/DBScripts/Data Import.sql
@@ -3,8 +3,8 @@ DECLARE @UserId UNIQUEIDENTIFIER = '1F6388E7-49F1-48E3-9F58-14F657CB0D0D'
 DECLARE @RoleId UNIQUEIDENTIFIER
 
 IF NOT EXISTS(Select * FROM AspNetUsers Where UserName ='Admin')
-	INSERT INTO AspNetUsers(Id, UserName,PasswordHash,NormalizedUserName,AccessFailedCount,EmailConfirmed,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnabled) Values
-	(@UserId,'Admin','AQAAAAIAAYagAAAAEKTZ/LyaD/jMUOPVuKBVLkdaXActb20JNAnuG3ODBO/1H65ORgWAySm0GdnC1UJgxQ==','Admin User',0,1,1,0,0);
+	INSERT INTO AspNetUsers(Id, UserName,Email,PasswordHash,NormalizedUserName,AccessFailedCount,EmailConfirmed,PhoneNumberConfirmed,TwoFactorEnabled,LockoutEnabled) Values
+	(@UserId,'Admin','admin@gmail.com','AQAAAAIAAYagAAAAEKTZ/LyaD/jMUOPVuKBVLkdaXActb20JNAnuG3ODBO/1H65ORgWAySm0GdnC1UJgxQ==','Admin User',0,1,1,0,0);
 
 --AspNetRoles Table Data Import
 IF NOT EXISTS(Select * FROM AspNetRoles Where Name ='Admin')
